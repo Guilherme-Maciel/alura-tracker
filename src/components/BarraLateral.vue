1cf07419ba06e7c3d0dfa592bdc34614629b04e5
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2022-08-08 15:08:05 -0300

    Feature: Configurações iniciais, instalação de dependências (Bulma e Font Awesome), componentes de Barra Lateral e Formulário.

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="Alura Tracker">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+//Exporta o componente, como no ReactJS
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+
+</script>
+
+<style scoped>
+header{
+    background: #0d3b66;
+    height: 100vh;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+}
+@media only screen and (max-width: 768px){
+    header{
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Disponibiliza todos os icones do Font Awesome
+import '@fortawesome/fontawesome-free/css/all.css'
+
 createApp(App).mount('#app')
