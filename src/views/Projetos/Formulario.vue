f8b9f1dc7362501de4b963d8feffc7557397015a
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2022-12-29 14:51:14 -0300

    Refactored: type of mutations by export const in ts file; Feature: delete Project

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -19,6 +19,7 @@
 
 <script lang="ts">
 import {useStore} from '@/store'
+import { ADICIONA_PROJETO, ALTERA_PROJETO } from '@/store/tipo-mutacoes';
 import { defineComponent} from 'vue';
 
 export default defineComponent({
@@ -46,14 +47,14 @@ export default defineComponent({
         salvar(){
             if(this.id){
                 //Editar
-                this.store.commit('ALTERA_PROJETO', {
+                this.store.commit(ALTERA_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
                 })
             }
             else{
                 //Faz o uso da mutation que criamos na store
-                this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             
             this.nomeDoProjeto = '';
