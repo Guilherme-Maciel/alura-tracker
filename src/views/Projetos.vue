a7e5538b6f32b6daf3603298a1fff1200b0553ad
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2022-12-29 14:35:12 -0300

    Feature: edit a Project; Refactored: component Formulario and Lista from project, implemented children routes in Projetos

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,67 +1,18 @@
 <template>
-    <section class="projetos">
+    <div class="projetos">
         <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label">
-                    Nome do Projeto
-                </label>
-                <input type="text" v-model="nomeDoProjeto" class="input" id="nomeDoProjeto">
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>
-        <table class="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!--Para cada projeto na lista de projetos...-->
-                <tr v-for="projeto in projetos" :key="projeto.id">
-                    <td>{{projeto.id}}</td>
-                    <td>{{projeto.nome}}</td>
-                </tr>
-            </tbody>
-
-        </table>
-    </section>
+        <!--Indica onde deve ser renderizada as rotas filhas-->
+        <router-view></router-view>
+    </div>
 </template>
 
-<script lang="ts">
-import {useStore} from '@/store'
-import { defineComponent, computed } from 'vue';
+<script>
+
+import {defineComponent} from 'vue'
 
 export default defineComponent({
-    name: 'ProjetosVue',
-    data(){
-        return {
-            nomeDoProjeto: '',
-        };
-    },
-    methods:{
-        //Método que preenche as informações de um projeto de acordo com a interface (id e nome)
-        salvar(){
-            //Faz o uso da mutation que criamos na store
-            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-            this.nomeDoProjeto = ''
-        }
-    },
-    setup(){
-        const store = useStore()
-        return {
-            store,
-            //Fica de olho quando a lista de projetos mudar
-            projetos: computed(() => store.state.projetos)
-        }
-    }
+    name: 'ProjetosVue'
 })
-
 </script>
 
 <style scoped>
